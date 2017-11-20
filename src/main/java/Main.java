112e53be4e1cc23250d7d0504ace86ad5c607763
brodan <christopher.hranj@gmail.com>
2017-11-20 01:13:45 -0500

    add token generator file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-
 import io.javalin.Javalin;
 
 public class Main {
+
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
+    public static final String TWILIO_NUMBER = "+15555555555";
+
     public static void main(String[] args) {
         Javalin app = Javalin.start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/token", ctx -> {
+            ctx.result("Token");
+        });
     }
 }
